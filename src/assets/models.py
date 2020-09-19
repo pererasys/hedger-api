a5ca7ee9612a61caf118c93fc22099cabe99b2fa
Andrew Perera <pererasys@gmail.com>
2020-09-19 17:45:10 -0400

    update asset models

--- src/assets/models.py
+++ src/assets/models.py
@@ -4,15 +4,14 @@ Copyright 2020
 '''
 
 from django.db import models
+from datetime import datetime
 import uuid
 
 class Asset(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ticker = models.CharField(max_length=10, unique=True)
+    symbol = models.CharField(max_length=10, unique=True)
+    exchange = models.CharField(max_length=50, blank=True)
     display_name = models.CharField(max_length=255)
-    bid = models.DecimalField(max_digits=9, decimal_places=2)
-    ask = models.DecimalField(max_digits=9, decimal_places=2)
-    last = models.DecimalField(max_digits=9, decimal_places=2)
 
 
     class Meta:
@@ -21,3 +20,18 @@ class Asset(models.Model):
 
     def __str__(self):
         return self.display_name
+
+class DailyReport(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE, related_name="reports")
+    open = models.DecimalField(max_digits=9, decimal_places=2)
+    high = models.DecimalField(max_digits=9, decimal_places=2)
+    low = models.DecimalField(max_digits=9, decimal_places=2)
+    close = models.DecimalField(max_digits=9, decimal_places=2)
+    volume = models.IntegerField()
+    adj_open = models.DecimalField(max_digits=9, decimal_places=2)
+    adj_high = models.DecimalField(max_digits=9, decimal_places=2)
+    adj_low = models.DecimalField(max_digits=9, decimal_places=2)
+    adj_close = models.DecimalField(max_digits=9, decimal_places=2)
+    adj_volume = models.IntegerField()
+    date = models.DateTimeField(default=datetime.now)
