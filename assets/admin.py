9fa58227f4582c9870fdd7f4a7f4ecb13c6c609d
Andrew Perera <pererasys@gmail.com>
2020-09-21 15:29:16 -0400

    Update admin

--- assets/admin.py
+++ assets/admin.py
@@ -1,6 +1,26 @@
+'''
+Written by Andrew Perera
+Copyright 2020
+'''
+
 from django.contrib import admin
 from .models import Exchange, Asset, DailyReport
 
-admin.site.register(Exchange)
-admin.site.register(Asset)
+class ExchangeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'acronym', 'total_assets']
+
+    def total_assets(self, obj):
+        return obj.assets.count()
+
+    total_assets.short_description = "total assets"
+
+class AssetAdmin(admin.ModelAdmin):
+    ordering = ['-active']
+    search_fields = ['symbol', 'name']
+    list_display = ['name', 'symbol', 'exchange', 'active']
+
+
+
+admin.site.register(Exchange, ExchangeAdmin)
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(DailyReport)
